6eeda65b596492e071ae480c03900240dc34a41b
JeremySBRN <jeremiassebrian7@gmail.com>
2026-05-27 22:42:44 -0300

    punto d y c2

--- Form1.cs
+++ Form1.cs
@@ -17,18 +17,30 @@ namespace Figuras
 
             figuras = new Figura[3]
             {
-                new Circulo(60),
-                new Rectangulo(30, 50),
-                new Cuadrado(45),
+                new Circulo(30),
+                new Rectangulo(60, 80),
+                new Cuadrado(110),
             };
         }
 
         private Color ColorAleatorio()
         {
-            int rojo = random.Next(0, 256);
-            int verde = random.Next(0, 256);
-            int azul = random.Next(0, 256);
-            return Color.FromArgb(rojo, verde, azul);
+            Color color;
+            int brillo;
+
+            do
+            {
+                int rojo = random.Next(0, 256);
+                int verde = random.Next(0, 256);
+                int azul = random.Next(0, 256);
+
+                color = Color.FromArgb(rojo, verde, azul);
+
+                brillo = (int)(0.299 * rojo + 0.587 * verde + 0.114 * azul);
+            }
+            while (brillo > 180);
+
+            return color;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -42,13 +54,15 @@ namespace Figuras
                 ColorAleatorio(),
             };
 
-            for (int i = 0; i < figuras.Length; i++)
+            int separacion = 120;
+
+        for (int i = 0; i < figuras.Length; i++)
+        {
+            using (Pen pen = new Pen(colores[i]))
             {
-                using (Pen pen = new Pen(colores[i]))
-                {
-                    figuras[i].Dibujar(pen, gr, i * 100, 50);
-                }
+                figuras[i].Dibujar(pen, gr, i * separacion, 50);
             }
+        }
         }
     }
 }
